dc40066e196bf8b126b514a58d0bd29e9a7d5524
Prins Kamariya <kamriyaprince@gmail.com>
2023-01-26 12:59:22 +0530

    basic api using get post and postman

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.json({
+    name: "Prins",
+    email: "prins@gmail.com",
+    password: "4532",
+  });
 });
 
 app.post("/api/v1/register", (req, res) => {
-  res.send(`<h1>Hello ${req.body.name} and your email is ${req.body.email}`);
-  console.log(req.body);
+  const userName = req.body.name;
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
+  res.json({
+    success: true,
+    name: userName,
+    email: userEmail,
+    password: userPassword,
+  });
 });
 
 app.listen(port, () => {
